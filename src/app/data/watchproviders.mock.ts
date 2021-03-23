dedd1542cff96462d19c69dab2e3a09883ba06e0
heather.cole <heathercole.va@gmail.com>
2021-03-23 17:24:13 -0400

    start of filter list

---
+++ src/app/data/watchproviders.mock.ts
@@ -0,0 +1,36 @@
+const WATCH_PROVIDERS = [
+    {
+        "provider_id": 10,
+        "provider_name": "Amazon Video"
+    },
+    {
+        "provider_id": 2,
+        "provider_name": "Apple iTunes"
+    },
+    {
+        "provider_id": 192,
+        "provider_name": "YouTube"      
+    },
+    {
+        "provider_id": 3,
+        "provider_name": "Google Play Movies"
+    },
+    {
+        "provider_id": 68,
+        "provider_name": "Microsoft Store"
+    },
+    {
+        "provider_id": 8,
+        "provider_name": "Netflix"
+    },
+    {
+        "provider_id": 15,
+        "provider_name": "Hulu"
+    },
+    {
+        "provider_id": 384,
+        "provider_name": "HBO Max"
+    }
+];
+
+export default WATCH_PROVIDERS;
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import GENRES from './../../data/genres.mock'
+
+// import { FilterService } from '../filter.service';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  filters: any[] = [];
+
+  // constructor(private filterService: FilterService) { }
   constructor() { }
 
   ngOnInit(): void {
+    this.getFilters();
+  }
+
+  getFilters(): void {
+    // this.filterService.getFilters()
+    // .subscribe((response: any) => {
+    //   this.filters = response.results;
+    // });
+    this.filters = GENRES;
   }
 
 }
+
