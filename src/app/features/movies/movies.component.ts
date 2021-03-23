5bed311d04088148dcb71d44ed5ac4ac1bcae523
Alexandra Screven <alexandra.screven@gsa.gov>
2021-03-23 18:49:57 -0400

    added movie genres

--- src/app/features/movies/movies.component.ts
+++ src/app/features/movies/movies.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { find } from 'lodash';
 import { MoviesService } from 'src/app/features/movies/movies.service';
+import { IGenre } from 'src/app/models/genres.model';
 import { Movie } from './movies.model';
 
 @Component({
@@ -8,8 +10,8 @@ import { Movie } from './movies.model';
   styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
-  
   movies: Movie[];
+  genres: IGenre[];
 
   constructor(private movieService: MoviesService) {}
 
@@ -17,6 +19,21 @@ export class MoviesComponent implements OnInit {
     this.movieService.getMovies()
       .subscribe(response => {
         this.movies = response.results;
+        this.getMovieGenres();
+      })
+  }
+
+  getMovieGenres() {
+    this.movieService.getMovieGenres()
+      .subscribe(response => {
+        this.genres = response.genres;
+        this.movies.forEach((movie: Movie) => {
+          movie.genres = [];
+          movie.genre_ids.forEach((id) => {
+            const genre = find(this.genres, {id: id})
+            movie.genres.push(genre);
+          })
+        });
       })
   }
 }
--- src/app/features/movies/movies.service.ts
+++ src/app/features/movies/movies.service.ts
@@ -3,11 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
+import { IGenre } from 'src/app/models/genres.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
+  genres: IGenre[];
   constructor(private http: HttpClient) {}
 
   // TODO: add response type
@@ -15,5 +17,11 @@ export class MoviesService {
     return this.http.get<any>(
       `${environment.movieDbUrl}/discover/movie/?api_key=${environment.movieDbKey}`
     );
+  }
+
+  getMovieGenres(): Observable<any> {
+    return this.http.get<any>(
+      `${environment.movieDbUrl}/genre/movie/list?api_key=${environment.movieDbKey}`
+    )
   }
 }
