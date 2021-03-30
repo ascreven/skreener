83d1ea64eb7ef9438c3cfe2833c6485fa91f4abe
Alexandra Screven <alexandra.screven@gsa.gov>
2021-03-30 18:34:05 -0400

    added filter visibility toggle

--- src/app/features/movies/movie-filter-resolver.service.ts
+++ src/app/features/movies/movie-filter-resolver.service.ts
@@ -1,31 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
 import { Observable, of }  from 'rxjs';
-import { MoviesService } from 'src/app/features/movies/movies.service';
-import { IGenre } from 'src/app/models/genres.model';
-import WATCH_PROVIDERS from './../../data/watch-providers.mock';
 
-type IMovieFilters = {
-  genres: IGenre[],
-  watchProviders: any[]
-}
+import { MoviesService } from 'src/app/features/movies/movies.service';
+import { Filter } from 'src/app/models/filters.model';
+import WATCH_PROVIDERS from './../../data/watch-providers.mock';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieFilterResolverService implements Resolve<any> {
 
-  // filters: IMovieFilters = {
-  //   genres: [],
-  //   watchProviders: WATCH_PROVIDERS
-  // }
-  filters = [];
+  filters: Filter[] = [
+    {
+      options: WATCH_PROVIDERS,
+      title: "Watch Providers",
+      isVisible: true,
+      name: "provider_name",
+      id: "provider_id"
+    }
+  ];
 
   constructor(private ms: MoviesService) { }
   resolve(): Observable<any> | Observable<never> {
     this.ms.getMovieGenres()
-      .subscribe(response => {
-        this.filters.push(response.genres);
+      .subscribe((response) => {
+        const filter = new Filter(response.genres, "Genres")
+        this.filters.push(filter);
     });
     return of(this.filters);
   }
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,36 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Filter } from 'src/app/models/filters.model';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-  filters: any[] = [];
+  filters: Filter[] = [];
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const data = this.route.snapshot.data;
-
-    this.getFilters(data.filters);
-  }
-
-  getFilters(dataFilters: any) {
-    console.log(dataFilters);
-    dataFilters.forEach(element => {
-      console.log(element);
-      const filter = {
-        title: "Genres",
-        options: element,
-        show: false
-      };
-      this.filters.push(filter);
-    });
+    this.filters = data.filters
   }
 
   dropdownToggle(filter: any) {
-    filter.show = !filter.show;
+    filter.isVisible = !filter.isVisible;
   }
 }
