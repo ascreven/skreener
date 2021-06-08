af0b0ede3fe66ab2b9426c2469715bdab8e296c5
Alexandra Screven <alexandra.screven@gsa.gov>
2021-06-08 17:08:27 -0400

    added material design theme

--- src/app/layout/results/results.module.ts
+++ src/app/layout/results/results.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatButtonModule} from '@angular/material/button';
 
 import { ResultsComponent } from './results.component';
 
 @NgModule({
   declarations: [ResultsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatButtonModule
   ],
   exports: [
     ResultsComponent
